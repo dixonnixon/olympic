72bc033f22d6062cb457b96940be557bc6611487
dixonnixon <drenb@ukr.net>
2018-11-20 04:14:47 +0200

    comm2

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database(':memory:', (err) =>{
-    if(err) {
-        console.log(err);
-    }
+let db = new sqlite3.Database(
+    './db/olympic_history.db', 
+    sqlite3.OPEN_READWRITE,
+    (err) =>{
+        if(err) {
+            console.log(err);
+        }
+    });
+    
+//SELECT name FROM my_db.sqlite_master WHERE type='table';
+db.serialize(() => {
+    db.each(
+        `SELECT name FROM sqlite_master WHERE type='table'`,
+        (err, row) => {
+            if(err) {
+                return console.error(err.message);
+            }
+            console.log(row);                       
+        }
+    );
 });
 
 db.close((err) => {
-    return console.log(err.message);
+    if(err) {
+        return console.error(err.message);
+    }
+    console.log("Closed");
 });
