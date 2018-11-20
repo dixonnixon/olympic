8ce1ed177b63d67576dcca9a7d8ac1be3dd15da2
dixonnixon <drenb@ukr.net>
2018-11-20 04:18:13 +0200

    Revert "comm2"
    This reverts commit 72bc033f22d6062cb457b96940be557bc6611487.

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database(
-    './db/olympic_history.db', 
-    sqlite3.OPEN_READWRITE,
-    (err) =>{
-        if(err) {
-            console.log(err);
-        }
-    });
-    
-//SELECT name FROM my_db.sqlite_master WHERE type='table';
-db.serialize(() => {
-    db.each(
-        `SELECT name FROM sqlite_master WHERE type='table'`,
-        (err, row) => {
-            if(err) {
-                return console.error(err.message);
-            }
-            console.log(row);                       
-        }
-    );
+let db = new sqlite3.Database(':memory:', (err) =>{
+    if(err) {
+        console.log(err);
+    }
 });
 
 db.close((err) => {
-    if(err) {
-        return console.error(err.message);
-    }
-    console.log("Closed");
+    return console.log(err.message);
 });
