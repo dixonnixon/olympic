6f577d12530e26c9551096e18537fb0f02640dad
dixonnixon <drenb@ukr.net>
2018-11-26 17:58:41 +0200

    connected to DB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database(':memory:', (err) =>{
+let db = new sqlite3.Database('./db/olympic_history.db', (err) =>{
     if(err) {
         console.log(err);
     }
